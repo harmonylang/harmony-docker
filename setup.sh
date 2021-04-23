f034057b263e49c63e187cd980ada794ba7e74d3
EC2 Default User <ec2-user@ip-172-31-29-15.us-east-2.compute.internal>
2021-04-23 14:35:27 +0000

    Update setup file to prune outdated images

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 sh ./update-harmony.sh
-docker build -t anthonyyang/harmony-docker .
+docker build --rm -t anthonyyang/harmony-docker .
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 rm -rf ./harmony-master
+
